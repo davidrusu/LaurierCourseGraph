2f73643d437aec606f9b95e44efcb3f6be7ac709
David Rusu <dave809@gmail.com>
2014-07-07 21:31:43 -0400

    add panning

--- depGraph.js
+++ depGraph.js
@@ -1,189 +1,258 @@
-Node[] nodes = new Node[]
-boolean drawing = False
-float x = 0;
-float y = 0;
-
-def setup() {
-  size(1300, 700)
-  for line in dot_file[1:-1] {
-    node_values = line.strip().split(' -> ')
-    node = node_values[0]
-    if not (node.startswith('CS')) {
-      continue
-    if node not in nodes_dict {
-      create_node(node)
-    if len(node_values) == 2 {
-      dep = node_values[1]
-      if dep not in nodes_dict {
-        create_node(dep)
-      nodes_dict[node].add_dep(nodes_dict[dep])
-
-def create_node(name) {
-  nodes_dict[name] = Node(name, x, y)
-  nodes.append(nodes_dict[name])
-  step = 20
-  x += step
-  if x > width {
-    x = 0
-    y += step
-  
-def keyPressed() {
-  drawing = not drawing
-  
-def update() {
-  
-  if True {
-    for i in range(len(nodes)) {
-    a = nodes[i]
-      depth = a.depth()
-      for j in range(i + 1, len(nodes)) {
-        b = nodes[j]
-        b_depth = b.depth()
-        k = -0.0001
-        rest = abs(depth - b_depth)**2 * 50 + 200
-        
-        dx = a.x - b.x
-        dy = a.y - b.y
-        dist = max(1, sqrt(dx * dx + dy * dy))
-        
-        
-        if not (b in a.dep or a in b.dep) {
-          k = 0.1
-          rest = 400
-          fx = 0.001 * (rest*3-dist) * dx / dist
-          fy = 0.0001 * (rest-dist) * dy / dist
-        else {
-		  #           if  {
-		  #             k *= 0.5
-#             rest += 500
-          fx = -k * (rest - dist) * dx / dist
-          fy = -k * (rest - dist) * dy / dist
-        a.vx += fx
-        a.vy += fy
-        b.vx -= fx
-        b.vy -= fy
-      
-  for node in nodes {
-    node.update_dep()
-
-  for node in nodes {
-    node.update()
+window.addEventListener('polymer-ready', function(e) {
+  function sketchProc(p) {
+    var nodes = [];
+    var nodeDict = {};
+    var drawing = true;
+    var scale = 1;
+    var shift = [0,0];
+    var lastMouse = [p.mouseX, p.mouseY]
+    var pointer = [p.mouseX, p.mouseY] // actual pointer position in 
+    var mousePressed = false;
     
-  if mousePressed {
-    for node in nodes {
-    dx = mouseX - node.x
-      dy = mouseY - node.y
-      dist = sqrt(dx*dx + dy*dy)
-      if dist < 20 {
-        node.x = mouseX
-        node.y = mouseY
-        break
-  
-def draw() {
-  update()
-  background(255)
-  min_x = width
-  min_y = height
-  max_x = 0
-  max_y = 0
-  sum_x = 0
-  sum_y = 0
-  n = 0.0
-  for node in nodes {
-    if node.x < min_x {
-    min_x = node.x
-    if node.y < min_y {
-      min_y = node.y
-    if node.x > max_x {
-      max_x = node.x
-    if node.y > max_y {
-      max_y = node.y
-    n += 1
-    sum_x += node.x
-    sum_y += node.y
-  span_x = max_x - min_x + 10.0
-  span_y = max_y - min_y + 10.0
-  avg_x = sum_x / n
-  avg_y = sum_y / n
-  center_x, center_y = width/2 - avg_x, height/2 - avg_y
-  s = min(width/span_x, height/span_y)
-  fill(0)
-    
-  
-  translate(width/2, height/2)
-  scale(s, s)
-  rect(avg_x, avg_y, 10, 10)
-  for node in nodes {
-    node.x -= avg_x
-    node.y -= avg_y
-  
-  for node in nodes {
-    node.draw()
-  
-class Node {
-  
-  def __init__(self, name, x, y) {
-    self.name = name
-    self.x = x
-    self.y = y
-    self.vx = 0
-    self.vy = 0
-    self.dep = []
-    self.parent = []
-    self.rate = random(20, 1000)
-    
-  def add_dep(self, node) {
-    self.dep.append(node)
-  
-  def update_dep(self) {
-    time = 10000
-    k = 0.001 + 0.1*(sin(millis() / self.rate) + 1) * max(0, time - millis()) / time
-    rest = 50 + 50 * sin(millis() / self.rate) * max(0, time-millis()) / time
-    for d in self.dep {
-      dx = d.x - self.x
-      dy = d.y - self.y
-      dist = sqrt(dx * dx + dy * dy)
-      length = (rest - dist)
-      fx = -k * length* dx / dist
-      fy = -k * length* dy / dist
-      self.vx += fx
-      self.vy += fy
-      d.vx -= fx
-      d.vy -= fy - 0.3
-  
-  def update(self) {
-    self.x += self.vx
-    self.y += self.vy
-    air = 0.9
-    self.vx *= air
-    self.vy *= air
-    
-    self.vy -= self.depth() * 0.01
-  
-  def depth(self) {
-    if len(self.dep) == 0 {
-    return 1
-    return max(map(Node.depth, self.dep)) + 1
-  
-  def draw(self) {
-    stroke(0)
-    fill(255)
-    ellipse(self.x, self.y, 10, 10)
-    if not drawing {
-      return
-    fill(0)
-    text(self.name, self.x, self.y) 
-    for d in self.dep {
-      arrow(d.x, d.y, self.x, self.y)
+    p.setup = function() {
+      p.frameRate(60);
+    }
 
-def arrow(x1, y1, x2, y2) {
-  line(x1, y1, x2, y2)
-  dx, dy = x2-x1, y2-y1
-  pushMatrix()
-  translate(x1 + dx / 2, y1 + dy / 2)
-  a = atan2(-dx, dy)
-  rotate(a)
-  r = 5
-  fill(0)
-  triangle(0, 0, -r/2, -r, r/2, -r)
-  popMatrix()
+    p.mousePressed = function() {
+      mousePressed = true;
+    }
+
+    p.mouseReleased = function() {
+      mousePressed = false;
+      shift[0] = shift[0]  + p.mouseX - startMouseX;
+      shift[1] = shift[1]  + p.mouseY - startMouseY;
+    }
+
+    function updateMouse() {
+      if (mousePressed) {
+        shift[0] += p.mouseX - lastMouse[0]
+        shift[1] += p.mouseY - lastMouse[1]
+      }
+      lastMouse[0] = p.mouseX;
+      lastMouse[1] = p.mouseY;
+    }
+
+    function update() {
+      reset();
+      updateMouse();
+      for (var i=0; i < nodes.length; i++) {
+        var a = nodes[i];
+        var depth = a.depth();
+        for (var j=i+1; j < nodes.length; j++) {
+          var b = nodes[j];
+          var b_depth = b.depth();
+          var k = -0.0001;
+          var depth_dist = p.abs(depth - b_depth);
+          var rest = depth_dist*depth_dist * 50 + 200;
+
+          var dx = a.x - b.x;
+          var dy = a.y - b.y;
+          var dist = p.max(1, p.sqrt(dx * dx + dy * dy));
+
+          if (!(a.children.indexOf(b) ||
+                b.children.indexOf(a))) {
+            var k = 0.1;
+            var rest = 400;
+            var fx = 0.001 * (rest*3-dist) * dx / dist;
+            var fy = 0.0001 * (rest-dist) * dy / dist;
+          } else {
+            var fx = -k * (rest - dist) * dx / dist
+            var fy = -k * (rest - dist) * dy / dist
+          }
+          a.vx += fx
+          a.vy += fy
+          b.vx -= fx
+          b.vy -= fy
+        }
+      }
+      for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        node.update();
+      }
+    }
+
+    function reset() {
+      if (!courses ||  !refresh) {
+        return;
+      }
+      nodes = [];
+      nodeDict = {};
+      console.log(nodeDict)
+      console.log('refresh')
+      var keys = Object.keys(courses);
+      for (var i = 0; i < keys.length; i++) {
+        var course_name = keys[i];
+        createNode(course_name);
+      }
+      refresh = false;
+    }
+
+    function createNode(key) {
+      if (nodeDict[key] != undefined) {
+        return nodeDict[key];
+      }
+      var course = courses[key];
+      var node = new Node(key, p.random(p.width), p.random(p.height));
+      nodeDict[key] = node;
+      nodes.push(node);
+      var children = course.children;
+      for (var i=0; i < children.length; i++) {
+        child = children[i];
+        node.add_dep(createNode(child));
+      }
+      return node;
+    }
+
+    p.draw = function() {
+      p.size(w, h)
+      update()
+
+      p.background(255);
+      var min_x = p.width;
+      var min_y = p.height;
+      var max_x = 0;
+      var max_y = 0;
+      var sum_x = 0;
+      var sum_y = 0;
+      var n = 0.0;
+      for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        if (node.x < min_x) {
+          min_x = node.x
+        }
+        if (node.y < min_y) {
+          min_y = node.y;
+        }
+        if (node.x > max_x) {
+          max_x = node.x;
+        }
+        if (node.y > max_y) {
+          max_y = node.y;
+        }
+        n += 1;
+        sum_x += node.x;
+        sum_y += node.y;
+      }
+      var span_x = max_x - min_x;
+      var span_y = max_y - min_y;
+      var centerX = sum_x / n;//(max_x + min_x) / 2;
+      var centerY = sum_y / n;//(max_y + min_y) / 2;
+      var translateX = p.width/2 + shift[0];
+      var translateY = p.height/2 + shift[1];
+      /* var s = p.min(p.width/span_x, p.height/span_y) * 0.9; */
+      p.fill(0);
+      p.translate(translateX, translateY);
+      /* p.scale(s, s); */
+      for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        node.x -= centerX;
+        node.y -= centerX;
+      }
+
+      for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        node.draw();
+      }
+
+    };
+
+   
+    function Node(name, x, y) {
+      this.name = name;
+      this.x = x;
+      this.y = y;
+      this.vx = 0;
+      this.vy = 0;
+      this.children = [];
+      this.parent = [];
+      this.rate = p.random(20, 1000);
+
+      this.add_dep = function(node) {
+        this.children.push(node);
+      };
+
+      this.update_dep = function(self) {
+        time = 10000;
+        angle = p.sin(millis() / self.rate);
+        timeScale = p.max(0, time - p.millis()) / time;
+
+        k = 0.001 + 0.1*(angle+1)*timeScale;
+        rest = 50 + 50*angle*timeScale;
+
+        for (var i = 0; i < this.children.length; i++) {
+          var d = this.children[i];
+          var dx = d.x - this.x;
+          var dy = d.y - this.y;
+          var dist = p.sqrt(dx * dx + dy * dy);
+          var length = rest - dist;
+          var vec =-k * length / dist ;
+          var fx = vec * dx;
+          var fy = vec * dy;
+          this.vx += fx;
+          this.vy += fy;
+          d.vx -= fx;
+          d.vy -= fy - 0.3; // we push dependencies down
+        }
+      }
+      this.update = function() {
+        this.x += this.vx;
+        this.y += this.vy;
+
+        var air = 0.9;
+        this.vx *= air;
+        this.vy *= air;
+        // nodes with lots of children float to the top
+        this.vy -= this.depth() * 0.01; 
+      }
+
+      this.depth = function() {
+        if (this.children.length == 0) {
+          return 1;
+        }
+
+        var max_depth = 0;
+        for (var i = 0; i < this.children.length; i++) {
+          var d = this.children[i];
+          var ddepth = d.depth();
+          if (ddepth > max_depth) {
+            max_depth = ddepth;
+          }
+        }
+        return max_depth + 1;
+      };
+      this.draw = function() {
+        p.stroke(0);
+        p.fill(255);
+        p.ellipse(this.x, this.y, 10, 10);
+        if (!drawing) {
+          return;
+        }
+        p.fill(0);
+        p.text(this.name, this.x, this.y);
+        for (var i = 0; i < this.children.length; i++) {
+          var d = this.children[i];
+          arrow(d.x, d.y, this.x, this.y);
+        }
+      };
+    };
+  };
+ 
+  var arrow = function(x1, y1, x2, y2) {
+    p.fill(0, 0, 0, 50);
+    p.stroke(0, 0, 0, 50);
+    p.line(x1, y1, x2, y2);
+    var dx = x2-x1;
+    var dy = y2-y1;
+    p.pushMatrix();
+    p.translate(x1 + dx / 2, y1 + dy / 2);
+    var a = p.atan2(-dx, dy);
+    p.rotate(a);
+    var r = 5;
+    p.fill(0);
+    p.triangle(0, 0, -r/2, -r, r/2, -r);
+    p.popMatrix();
+  };
+
+  var canvas = document.getElementById("mycanvas");
+  var p = new Processing(canvas, sketchProc);
+});
